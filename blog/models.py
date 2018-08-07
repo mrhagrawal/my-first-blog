1065da63dd64a2a845448fd9e974b35ea40110d0
mrhagrawal <[mr.hagrawal@gmail.com]>
2018-08-07 17:23:19 +0530

    Changed HTML for site. Infact added it for the first time.PS It is blank as of now

--- blog/models.py
+++ blog/models.py
@@ -9,7 +9,7 @@ class Post(models.Model):
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
-            default=timezone.now,blank=True, null=True)
+            blank=True, null=True)
 
     def publish(self):
         self.published_date = timezone.now()
@@ -17,6 +17,4 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-
-
 # Create your models here.
